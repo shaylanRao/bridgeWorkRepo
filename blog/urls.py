326708a86498d72f8ade52bcdfb19f7e7e3e8639
shaylanRao <sxr980@student.bham.ac.uk>
2020-05-22 21:03:26 +0100

    problem in get_homepage call

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', 					views.get_homepage, 	name='get_homepage'),
+    path('', views.get_homepage, name='get_homepage'),
     path('post/<int:pk>/', 		views.post_detail, 		name='post_detail'),
     path('post/new/', 			views.post_new, 		name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, 		name='post_edit'),
